e4357278678c35a3ca7e353e33ec3fb15187bef0
Jaswanth1410 <jaswanthsaikakavakam@gmail.com>
2022-07-21 18:15:42 +0530

    team_page

---
+++ src/Components/Pages/FlipCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+const FlipCard = (con) => {
+  // const image = {
+  //   background: linear-gradient(rgba(0, 0, 0, 0.866),rgba(0, 0, 0, 0.806)), url(
+  // }
+  return (
+    
+    <div className="card">
+      <div style={{ backgroundImage: `url(${con.src})` }} className="card-face front"></div>
+      <div style={{ background: `linear-gradient(rgba(0, 0, 0, 0.866),rgba(0, 0, 0, 0.806)), url(${con.src})` }} className="card-face back">
+        <h2>{con.name}</h2>
+        <p>Peer Mentor</p>
+      </div>
+    </div>
+  
+  )
+}
+
+export default FlipCard
---
+++ src/Components/Pages/Team.js
@@ -0,0 +1,211 @@
+import React from 'react'
+import img1 from '../Images/myPic.jpg'
+import FlipCard from './FlipCard'
+import './Team.css'
+const Team = () => {
+  let content = [
+    {
+      id: 1,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 2,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 3,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 4,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 5,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 6,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 7,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 8,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 9,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 10,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 11,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 12,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 13,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 14,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 15,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 16,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 17,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 18,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 19,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 20,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 21,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 22,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 23,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 24,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 25,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 26,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 27,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 28,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 29,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 30,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 31,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+    {
+      id: 32,
+      src: img1,
+      name: "abc",
+      descp: "",
+    },
+  ];
+
+  return (
+    <div className="team">
+      {content.map((con) => (
+            <FlipCard key={con.id} src={con.src} name = {con.name} />
+          ))}
+    </div>
+    
+  )
+}
+
+export default Team
