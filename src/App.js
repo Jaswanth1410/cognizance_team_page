f32a23442b8d107e4c6b0a2cb8fb2f80c54d57a8
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2022-07-21 23:49:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-// import './App.css';
-import Team from './Components/Pages/Team.js';
+import React from "react";
+import './App.css';
+import Team from "./Components/Pages/Team.js";
 
 function App() {
   return (
