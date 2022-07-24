5c61039d251ca2be3d2e9ad02d17488eb57b5dfe
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2022-07-24 10:30:41 +0530

    Update FlipCard.js

--- src/Components/Pages/FlipCard.js
+++ src/Components/Pages/FlipCard.js
@@ -1,50 +1,61 @@
-import React from "react";
+// import React, { useState } from "react";
+import React from 'react'
 import Modal from "./Modal.js";
 
 const FlipCard = (con) => {
-  // const image = {
-  //   background: linear-gradient(rgba(0, 0, 0, 0.866),rgba(0, 0, 0, 0.806)), url(
-  // }
-
-  // function popUpImg() {
-  //   const ele = document.getElementById("popUp");
-  //   ele.classList.add("popUpImg");
-  // }
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  // const [model, setModel] = useState(false);
 
   return (
-    <div className="card">
-      {/* <div id="popUp"
-      //  onClick={{ popUpImg }}
-       >
-        <p style={{ textAlign: "left" }}>{con.name}</p>
-        <p style={{ textAlign: "left" }}>{con.post}</p>
-        <p style={{ textAlign: "left" }}>{con.dom}</p>
-        <p style={{ textAlign: "justify" }}>{con.desc}</p>
-      </div> */}
+    <>
       <div
-        style={{ backgroundImage: `url(${con.src})` }}
-        className="card-face front"
-      ></div>
-      <div
-        style={{
-          background: `linear-gradient(rgba(0, 0, 0, 0.866),rgba(0, 0, 0, 0.806)), url(${con.src})`,
-          // background: `url(${con.src})`,
+        className="card"
+        onClick={() => {
+          setIsModalOpen(true);
         }}
-        className="card-face back"
+        // onClick={() => {
+        //   setModel(true);
+        // }}
       >
-        <button>
-          {con.name}
-          <Modal
-            src={con.src}
-            name={con.name}
-            post={con.post}
-            dom={con.dom}
-            desc={con.desc}
-          />
-        </button>
-        <p>{con.post}</p>
+        <div
+          style={{ backgroundImage: `url(${con.src})` }}
+          className="card-face front"
+        ></div>
+        <div
+          style={{
+            background: `linear-gradient(rgba(0, 0, 0, 0.866),rgba(0, 0, 0, 0.806)), url(${con.src})`,
+          }}
+          className="card-face back"
+        >
+          <h2>{con.name}</h2>
+
+          <p>{con.post}</p>
+        </div>
       </div>
-    </div>
+      {/* <div className={ model ? "model open" : "model" }>
+        <Modal
+          src={con.src}
+          name={con.name}
+          post={con.post}
+          dom={con.dom}
+          desc={con.desc}
+        />
+      </div> */}
+      {isModalOpen && (
+        <Modal
+          src={con.src}
+          name={con.name}
+          post={con.post}
+          dom={con.dom}
+          desc={con.desc}
+          mail={con.mail}
+          gitHub={con.gitHub}
+          linkedIn={con.linkedIn}
+          insta={con.insta}
+          close={setIsModalOpen}
+        />
+      )}
+    </>
   );
 };
 
